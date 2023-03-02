be4a8e57e6b99bbf25754a95ca488c84852da12b
Sami <99116181+zvhh@users.noreply.github.com>
2023-03-02 16:30:53 +0300

    Add files via upload

--- setup.py
+++ setup.py
@@ -10,5 +10,34 @@ setup(
     packages=find_packages('src'),
     package_dir={'': 'src'},
     url='https://github.com/zvhh/Socket-Like-Requests',
-    keywords='simple socket'
+    keywords='simple socket',
+)
+
+
+from distutils.core import setup
+setup(
+  name = 'socket like requests',
+  packages = ['socket like requests'],
+  version = '0.1',
+  license='MIT',
+  description = 'Use Socket Like Requests Module',
+  author = 'Sami',         
+  author_email = 'sami2700@outlook.com', 
+  url = 'https://github.com/zvhh/Socket-Like-Requests',
+  download_url = 'https://github.com/zvhh/Socket-Like-Requests/archive/refs/tags/0.1.tar.gz',   
+  keywords = ['Requests', 'TCP Request', 'Socket'],
+  install_requires=[          
+          'struct',
+          'base64',
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.11',
+  ],
 )
