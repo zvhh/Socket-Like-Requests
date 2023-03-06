4cc207f5161434d22217e9f7cc9c466bafc8a4f4
Sami <99116181+zvhh@users.noreply.github.com>
2023-03-06 22:06:27 +0300

    Add files via upload

--- socket_like_requests/slr.py
+++ socket_like_requests/slr.py
@@ -65,7 +65,7 @@ class REQ:
         
         user,pa = auth.split(':')
         auth_a = base64.b64encode(f'{user}:{pa}'.encode()).decode()
-        hed = f'CONNECT www.{self.path}:443 HTTP/1.1\r\nHost: www.{self.path}\r\nProxy-Authorization: Basic {auth_a}\r\n\r\n'.encode()
+        hed = f'CONNECT www.{self.path}:{self.port} HTTP/1.1\r\nHost: www.{self.path}\r\nProxy-Authorization: Basic {auth_a}\r\n\r\n'.encode()
         s.sendall(hed)
         
         response = s.recv(20)
@@ -76,7 +76,7 @@ class REQ:
     
     def https(self, s):
         
-        s.sendall(f'CONNECT www.{self.path}:443 HTTP/1.0\r\n\r\n'.encode())
+        s.sendall(f'CONNECT www.{self.path}:{self.port} HTTP/1.0\r\n\r\n'.encode())
         response = s.recv(20)
         #print(response)
         
@@ -84,7 +84,7 @@ class REQ:
         
     def socks4(self, s):
         
-        s.sendall(b'\x04\x01' + struct.pack('>H', 443) + socket.inet_aton(socket.gethostbyname(f'www.{self.path}')) + b'\x00')
+        s.sendall(b'\x04\x01' + struct.pack('>H', self.port) + socket.inet_aton(socket.gethostbyname(f'{self.path}')) + b'\x00')
         response = s.recv(8)
         if response[0] != 0x00:
             raise Exception('Bad Proxy')
@@ -103,7 +103,7 @@ class REQ:
         
         ipz = socket.gethostbyname(f'www.{self.path}')
         
-        s.sendall(b'\x05\x01\x00\x03' + chr(len(ipz)).encode() + ipz.encode() + struct.pack('>H', 443))
+        s.sendall(b'\x05\x01\x00\x03' + chr(len(ipz)).encode() + ipz.encode() + struct.pack('>H', self.port))
         
         response = s.recv(10)
         
@@ -116,23 +116,59 @@ class REQ:
     
     def verify(self, s):
         
-        s = ssl.create_default_context().wrap_socket(s, server_hostname=f'{self.path}')
+        if self.port == 443:
+        
+            s = ssl.create_default_context().wrap_socket(s, server_hostname=f'{self.path}')
     
         return s
 
-    def path_de(self, path, hh=None):
+    def path_de(self, path, hh=None, po=None):
         
-        z = path.split('/')
+        #print(path)
+        
+        ho, xz = path.split('://')
+       # print(ho)
+        
+        if ho.__contains__('https'):
+            po = 443
+            #print('port = 443')
+            
+        elif ho.__contains__('http'):
+            #print('port = 80')
+            po = 80
+        
+        z = xz.split('/')
         host = z[0]
         
         z.remove(z[0])
         hh = '/'.join(z)
         
-        return host, hh
-    
-    def post(self, path, hed=None, data=None, proxy=None, verify=None, proxy_type=None, auth=None, timeout=5, text=None):
         
-        self.path, hh = self.path_de(path)
+        
+        hh = '/' + hh
+        
+        return host, hh, po
+    
+    
+    def get(self, path, **kwargs):
+        
+        r''''''
+        
+        return self.send(path, 'GET', **kwargs)
+    
+    
+    def post(self, path, **kwargs):
+        
+        r''''''
+        
+        return self.send(path, 'POST', **kwargs)
+    
+        
+    def send(self, path ,method=None, hed=None, data=None, proxy=None, verify=None, proxy_type=None, auth=None, timeout=5):
+        
+        self.path, hh, self.port = self.path_de(path)
+        
+        #print(self.port)
 
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0)
 
@@ -144,8 +180,12 @@ class REQ:
         
         if verify == None or verify == True:
             s = self.verify(s)
-            
-        hed = f'POST /{hh} HTTP/1.1\r\nHost: {self.path}\r\nContent-Length: {len(self.data(data))}\r\n{self.hed(hed)}\r\n{self.data(data)}'.encode()
+
+        if proxy_type.__contains__('http'):
+            hh = path
+
+
+        hed = f'{method} {hh} HTTP/1.1\r\nHost: {self.path}\r\nContent-Length: {len(self.data(data))}\r\n{self.hed(hed)}\r\n{self.data(data)}'.encode()
         #print(hed)
         s.sendall(hed)
         
@@ -169,7 +209,12 @@ class REQ:
         
         
         return Response(qz)
+        
     
+    
+class met:
+    
+    pass
     
     
 class Response:
@@ -188,9 +233,9 @@ class Response:
     @property
     def sc(self):
         
-        sc = self.qz.splitlines()[0]
+        status_code = int(self.qz.splitlines()[0].split(' ')[1])
 
-        return sc
+        return status_code
     
 
 req = REQ()
